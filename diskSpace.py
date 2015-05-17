c1ce7e8ed614512e1e7562fbcde352cc23092984
Josh <ham@x303.com>
2015-05-17 14:58:27 -0400

    * Created diskSpaceCaller to test the diskSpace Functions * Created get_avalible_disks() function     It returns a list of either all disks or the "real" disks in the system

--- diskSpace.py
+++ diskSpace.py
@@ -1,6 +1,7 @@
 import ctypes
 import platform
 import os
+import win32api
 
 
 class HardDriveSpaceException(Exception):
@@ -12,8 +13,7 @@ class HardDriveSpaceException(Exception):
 
 
 def get_free_space(folder, units="MB"):
-
-    """ 
+    """
         Return folder/drive free space
         :rtype: (int, int, str)
         :type folder: str
@@ -25,19 +25,37 @@ def get_free_space(folder, units="MB"):
         total_bytes = ctypes.c_ulonglong(0)
         ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, ctypes.pointer(total_bytes),
                                                    ctypes.pointer(free_bytes))
-        return int(free_bytes.value / u_constants[units.upper()]), int(
-            total_bytes.value / u_constants[units.upper()]), units
+        return int(free_bytes.value / u_constants), int(
+            total_bytes.value / u_constants), units
     else:
-        return int(os.statvfs(folder).f_bfree * os.statvfs(folder).f_bsize / u_constants[units.upper()]), units
+        return int(os.statvfs(folder).f_bfree * os.statvfs(folder).f_bsize / u_constants), units
 
-def get_byte_unit_const(units = "MB"):
-    
+
+def get_byte_unit_const(units="MB"):
     unit_const = {"GB": 1073741824,
-               "MB": 1048576,
-               "KB": 1024,
-               "B": 1
-               }
+                  "MB": 1048576,
+                  "KB": 1024,
+                  "B": 1
+                  }
     return unit_const[units.upper()]
+
+
+def get_available_disks(only_real_drives=True):
+    drives = win32api.GetLogicalDriveStrings()
+    drives = drives.split("\x00")
+
+    if only_real_drives: 
+        real_drives = []
+        for drive in drives:
+            if os.path.isdir(drive):  # if drive is a directory
+                real_drives.append(drive)
+        if not real_drives:
+            return drives  # if real_drives is empty, lets return drives to prevent any problems.
+        else:
+            return real_drives
+
+    else:
+        return drives
 
 
 if __name__ == "__main__":
--- diskSpaceCaller.py
+++ diskSpaceCaller.py
@@ -1,7 +1,15 @@
 __author__ = 'Josh'
 import diskSpace
-
-print (diskSpace.get_free_space("C:\\","GB"))
-print (diskSpace.)
+# import win32api
+import os
+#
+# drives = win32api.GetLogicalDriveStrings()
+# drives = drives.split("\x00")
+drives = diskSpace.get_available_disks(True)
+for drive in drives:
+    # drive = drive.strip('\\')
+    # drive = drive.lower()
+    print drive
+    print os.path.isdir(drive)
+    print (diskSpace.get_free_space(drive, "GB"))
 raw_input("End")
-
